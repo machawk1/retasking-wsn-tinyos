50ba78efe6359e9587eb6dda04879fb1daedec16
Mike Ruffing <devnull@localhost>
2012-11-13 01:47:36 -0500

    Created the deluge-visualizer project (Maven) Updated the hgignore file

---
+++ support/deluge-visualizer/src/main/java/net/tinyos/dviz/App.java
@@ -0,0 +1,13 @@
+package net.tinyos.dviz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
