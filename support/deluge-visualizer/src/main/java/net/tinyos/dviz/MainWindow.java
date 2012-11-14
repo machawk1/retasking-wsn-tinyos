3c74808b7fa68cbc78940bdf719a08ffbc518647
Mike Ruffing <devnull@localhost>
2012-11-13 21:59:15 -0500

    Added two splitpanels (Node Status Table, Command Tabs, and Console)

--- support/deluge-visualizer/src/main/java/net/tinyos/dviz/MainWindow.java
+++ support/deluge-visualizer/src/main/java/net/tinyos/dviz/MainWindow.java
@@ -1,5 +1,7 @@
 package net.tinyos.dviz;
 
+import javax.swing.SwingUtilities;
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTable;
@@ -12,11 +14,17 @@ import javax.swing.JMenuItem;
 import java.awt.Component;
 import java.awt.Insets;
 import javax.swing.JSplitPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.ScrollPane;
+import javax.swing.JTextArea;
 
 public class MainWindow {
 
 	private JFrame frame;
-	private JTable table;
+	private JTable tbNodeStatus;
+	private JSplitPane spChild;
 
 	/**
 	 * Launch the application.
@@ -46,7 +54,7 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 596, 483);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JMenuBar menuBar = new JMenuBar();
@@ -57,27 +65,48 @@ public class MainWindow {
 
 		JMenuItem mntmSettings = new JMenuItem("Settings");
 		mnFile.add(mntmSettings);
-				
-				JScrollPane scrollPane = new JScrollPane();
-				frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-				
-				table = new JTable();
-				table.setModel(new DefaultTableModel(
-					new Object[][] {
-						{null, null, null, null, null, null},
-					},
-					new String[] {
-						"", "Node ID", "Group ID", "State", "App UID", "App Name"
-					}
-				) {
-					Class[] columnTypes = new Class[] {
-						Boolean.class, Integer.class, Integer.class, String.class, Integer.class, String.class
-					};
-					public Class getColumnClass(int columnIndex) {
-						return columnTypes[columnIndex];
-					}
-				});
-				scrollPane.setViewportView(table);
+
+		
+		tbNodeStatus = new JTable();
+		tbNodeStatus.setModel(new DefaultTableModel(new Object[][] { { null, null,
+				null, null, null, null }, }, new String[] { "", "Node ID",
+				"Group ID", "State", "App UID", "App Name" }) {
+			Class[] columnTypes = new Class[] { Boolean.class, Integer.class,
+					Integer.class, String.class, Integer.class, String.class };
+
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		
+		JScrollPane spTable = new JScrollPane();
+		spTable.setViewportView(tbNodeStatus);
+
+		spChild = new JSplitPane();
+		spChild.setDividerSize(7);
+		spChild.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		spChild.setOneTouchExpandable(true);
+		spChild.setResizeWeight(0.5);
+		
+		
+		spChild.setLeftComponent(spTable);
+		
+		JScrollPane spCommandTabs = new JScrollPane();
+		spChild.setRightComponent(spCommandTabs);
+
+		
+		JSplitPane spRoot = new JSplitPane();
+		spRoot.setDividerSize(7);
+		spRoot.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		spRoot.setOneTouchExpandable(true);
+		spRoot.setResizeWeight(0.66);
+		frame.getContentPane().add(spRoot, BorderLayout.CENTER);
+		
+		spRoot.setLeftComponent(spChild);
+		
+		JTextArea taConsole = new JTextArea();
+		spRoot.setRightComponent(taConsole);
+		
 	}
 
 }
